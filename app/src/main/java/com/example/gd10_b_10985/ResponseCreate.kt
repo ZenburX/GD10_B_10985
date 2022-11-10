7dc18478884d2b9336d4d7afc0d723d77ad6f0a2
ZenburX <rubenz.lexanois@gmail.com>
2022-11-10 20:21:50 +0700

    Codeigniter

--- app/src/main/java/com/example/gd10_b_10985/ResponseCreate.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gd10_b_10985
-
-import com.google.gson.annotations.SerializedName
-
-class ResponseCreate (
-    @SerializedName("status") val stt:Int,
-    @SerializedName("error") val e:Boolean,
-    @SerializedName("message") val pesan:String,
-)
--- app/src/main/java/com/example/gd10_b_10985/ResponseDataMahasiswa.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gd10_b_10985
-
-import com.google.gson.annotations.SerializedName
-
-data class ResponseDataMahasiswa(
-    @SerializedName("status") val stt:String,
-    val totaldata: Int,
-    val data:List<MahasiswaData>
-)
